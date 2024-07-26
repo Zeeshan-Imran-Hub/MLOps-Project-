60442823406dd29d8649b2cbd384f403951cc428
Zeeshan-Imran-Hub <eng.zeeshanimran@gmail.com>
2024-07-26 14:42:04 +0300

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="heart_disease_prediction",
+    version='0.0.0',
+    author='<Zeeshan Imran>',
+    author_email='<eng.zeeshanimran@gmail.com>',
+    packages=find_packages(),
+)
